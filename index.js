d57cd56529c40f0c4527a65141bc75b9312cd9ed
GarvSaxena <messagegarv32@gmail.com>
2026-04-22 20:42:07 +0530

    feat: add auto-lock system using phone detection and inactivity tracking

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const ping = require("ping");
+const iohook = require("@tkomde/iohook");
 const {exec} = require("child_process"); //This module lets your program run system commands (like CMD/Terminal commands)
 // {exec} means that we are only taking the exec function from child_process
-console.log("Script starting");
-const phoneip = "10.23.1.39";
-// async function logout(){
-//     const res = await ping.promise.probe(phoneip);
-//     if(!res.alive) {
-//         console.log("Phone not detected");
-//         exec("shutdown -l");
-//     }
-// }
-// const rescount = await ping.promise.probe(phoneip);
 
+const phoneip = "10.23.1.39";
 let failcount = 0;
 let locked = false;
-setInterval(async() => {
+let isIdle = false;
+
+let activitytime = Date.now();
+iohook.on("mousemove", () => activitytime = Date.now());
+iohook.on("keydown", () => activitytime = Date.now());
+iohook.on("keyup", () => activitytime = Date.now());
+iohook.on("mousedown", () => activitytime = Date.now());
+
+iohook.start();
+console.log("Script starting");
+
+setInterval(() => {
+    let idletime = Date.now() - activitytime;
+    console.log("Active")
+    if (idletime > 30000) {
+        isIdle = true;
+        console.log("Not active")
+    } else {
+        isIdle = false;
+    }
+
+}, 2000);
+
+    setInterval(async() => {
     const res = await ping.promise.probe(phoneip);
     console.log("ping result: " , res.alive);
     
@@ -27,10 +42,17 @@ setInterval(async() => {
         locked = false;
     }
 
-    if(failcount >=5 && !locked){
+    if(failcount >=3 && !locked && isIdle){
         locked = true;
         console.log("Phone not detected..Locking..");
         exec("rundll32.exe user32.dll,LockWorkStation");
     }
     
 },2000);
+
+
+
+
+
+
+
